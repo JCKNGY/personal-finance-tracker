fdf1de5eb0a3510c33e5d7dd241958fca3ecd517
JKN <jkncpp@gmail.com>
2026-07-02 17:17:20 -0500

    test for add

--- PersonalFinanceTracker/obj/Debug/net10.0/PersonalFinanceTracker.AssemblyInfo.cs
+++ PersonalFinanceTracker/obj/Debug/net10.0/PersonalFinanceTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalFinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0a7ab8e9f5222a8d7ca1b65c05402cd4d64a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c11c31a38514e84e4493f95433c2b3a3d4bec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalFinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalFinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
